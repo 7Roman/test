d47732e5690af1511df9065e8113504398d3b72c
Roman <7RomanGo@gmail.com>
2022-02-16 19:39:30 -0500

    2nd commit

--- src/git/testing.java
+++ src/git/testing.java
@@ -3,6 +3,8 @@ package git;
 public class testing {
 public static void main (String[]args) {
 	
-	System.out.print("What's Up");
-}
+	System.out.print("What's Up ");
+	System.out.print("Who are you");
+	}
+
 }
