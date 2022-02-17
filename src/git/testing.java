eec7afd2b622f6cbb5b2ae1582384e56910711a2
Roman <7RomanGo@gmail.com>
2022-02-16 20:24:58 -0500

    5th commit

--- src/git/testing.java
+++ src/git/testing.java
@@ -6,6 +6,9 @@ public static void main (String[]args) {
 	System.out.print("What's Up ");
 	System.out.print("Who are you ");
 	
-    System.out.print("I would like a drink ");
+   System.out.print("I would like a drink ");
+   
+   
+   System.out.print("YOLO ");
 }
 }
