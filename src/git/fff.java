b5011f06a95ba2ce32899a8bbb0b5390fd995fad
Roman <7RomanGo@gmail.com>
2022-02-17 22:08:04 -0500

    add code: fff.java

---
+++ src/git/fff.java
@@ -0,0 +1,15 @@
+package git;
+
+public class fff {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i=0;i<3;i++) {
+			for (int x=0;x<2;x++) { 
+			 
+			 }System.out.println("I am a great student !");
+				}
+	}
+
+}
